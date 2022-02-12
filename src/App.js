5e26be344bd3b1280fff5fc20ce986c197f63439
Oluwaniyi66 <oluwaniyiropo11@gmail.com>
2022-02-12 17:57:53 +0100

    my first ecommerce

--- src/App.js
+++ src/App.js
@@ -11,26 +11,27 @@ import Shipping from './screens/Shipping';
 import Login from './screens/Login';
 import Register from './screens/Register';
 import OrderCompleted from './screens/OrderCompleted';
+import Homepage from './screens/Homepage';
 
 function App() {
   return (
     <div className="App">
-       <BrowserRouter>
-       <NavBar/>
-       <Routes>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Homepage/>}/>
+          <Route path="/grid" element={<Grid />} />
+          <Route path="/list" element={<List />} />
+          <Route path="/left" element={<Left />} />
+          <Route path="/productdetail" element={<ProductsDetails />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/ship" element={<Shipping />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/ordercompleted" element={<OrderCompleted />} />
+        </Routes>
 
-         <Route path="/grid" element= {<Grid/>} />
-         <Route path="/list" element={<List/>}/>
-         <Route path="/left" element={<Left/>}/>
-         <Route path="/productdetail" element={<ProductsDetails/>}/>
-         <Route path="/cart" element={<Cart/>}/>
-         <Route path="/ship" element={<Shipping/>}/>
-         <Route path="/login" element={<Login />} />
-         <Route path="/register" element={<Register/>}/>
-         <Route path="/ordercompleted" element={<OrderCompleted/>}/>
-       </Routes>
-
-       </BrowserRouter>
+      </BrowserRouter>
     </div>
   );
 }
--- src/screens/Homepage.js
+++ src/screens/Homepage.js
@@ -1,12 +1,33 @@
-import React from 'react'
-
+import React from "react";
+import "./Homepage.css";
 
 function Homepage() {
-    return (
-        <div>
-            
+  return (
+    <div className="container hm-nav-bg">
+      <div className="col-lg-12 d-flex">
+        <div className="col-lg-2 hm-nav-img-bg">
+          <div className="hm-nav-img">
+            {" "}
+            <img src="./lamp.png" alt="" />
+          </div>
         </div>
-    )
+        <div className="col-lg-5">
+          <h2>Best Furniture For Your Castle....</h2>
+          <div className="">
+            <h3>New Furniture Collection</h3>
+            <h3>Trends in 2020</h3>
+          </div>
+          <div className="hm-nav-text4">
+          <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in est adipiscing
+in phasellus non in justo.</h6>
+
+          </div>
+            <button className="btn btn-danger col-lg-4 col-md-8 col-sm-12">Shop</button>
+        </div>
+        <div className="col-lg-5"></div>
+      </div>
+    </div>
+  );
 }
 
-export default Homepage
+export default Homepage;
